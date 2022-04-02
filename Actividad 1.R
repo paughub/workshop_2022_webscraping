62dbd84911c9b0ef71550c6461557e136f692286
Diego Muñoz <dimunozu@gmail.com>
2022-04-02 12:44:42 -0300

    primer web scraping

--- Actividad 1.R
+++ Actividad 1.R
@@ -14,11 +14,27 @@ valor_suma == valor_suma2
 # install.packages('tidyverse')
 # install.packages('rvest')
 # install.packages('RSelenium')
+# install.packages('dplyr')
 
+# library(dplyr)
+# library(rvest)
+# library(RSelenium)
 
-library(tidyverse)
+# Actividad 1 =========================
+
+library(dplyr)
 library(rvest)
-library(RSelenium)
 
+# 1) Encuentre el número de páginas de productos en oferta
+# Windows: control + shift + M
+# Mac: Comando + shift + M
+
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1#origin=qcat&filter_applied=promotion_type&filter_position=2')
+
+
+pagina %>% 
+  html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+  html_children() %>% 
+  html_text2()
 
 
