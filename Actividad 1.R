c7adbc2c06bf34f3390b9702bdc45696c2992285
Diego Muñoz <dimunozu@gmail.com>
2022-04-02 12:00:58 -0300

    .

---
+++ Actividad 1.R
@@ -0,0 +1,16 @@
+exp(20)
+log(20,base = 2)
+
+
+valor_suma  = 2+2
+valor_suma2 <- 2+5
+
+valor_suma == valor_suma2
+
+# Librerias --
+# Son paquetes de funciones que dispone la comunidad para el uso gratuito de estas
+# Ejemplos de estas son tidyverse, rvest, RSleneium
+
+install.packages('tidyverse')
+install.packages('rvest')
+install.packages('RSleneium')
