b674a35763a0fd8e17da159728550f591c8ba179
Diego Muñoz <dimunozu@gmail.com>
2022-04-09 11:33:22 -0400

    new price

--- Actividad 1.R
+++ Actividad 1.R
@@ -1,35 +1,31 @@
-exp(20)
-log(20,base = 2)
-
-
-valor_suma  = 2+2
-valor_suma2 <- 2+5
-
-valor_suma == valor_suma2
-
 # Librerias --
 # Son paquetes de funciones que dispone la comunidad para el uso gratuito de estas
 # Ejemplos de estas son tidyverse, rvest, RSleneium
 
 # install.packages('tidyverse')
-# install.packages('rvest')
 # install.packages('RSelenium')
 # install.packages('dplyr')
+# install.packages('rvest')
+# install.packages('stringr')
+
 
 # library(dplyr)
 # library(rvest)
 # library(RSelenium)
+# library(stringr)
+
 
 # Actividad 1 =========================
 
-
-
 library(dplyr)
 library(rvest)
+library(stringr)
 
 # 1) Encuentre el número de páginas de productos en oferta
 # Windows: control + shift + M
 # Mac: Comando + shift + M
+
+
 
 pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1#origin=qcat&filter_applied=promotion_type&filter_position=2')
 
@@ -43,5 +39,31 @@ N_Paginas <- pagina %>%
   suppressWarnings()
 
 
+Nombre <- pagina %>% 
+  html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+  html_text2()
 
+
+oldprice <- pagina %>% 
+  html_elements(xpath = '//span[@class = "promotion-item__oldprice"]') %>% 
+  html_text2() %>% 
+  str_remove_all('\\$ ') %>% 
+  str_remove_all('\\.') %>% 
+  as.numeric()
+
+
+newprice <- pagina %>% 
+  html_elements(xpath = '//span[@class = "promotion-item__price"]') %>% 
+  html_text2() %>% 
+  str_remove_all('\\$ ') %>% 
+  str_remove_all('\\.') %>% 
+  as.numeric()
+
+
+pagina %>% 
+  html_elements(xpath = '//span[@class = "promotion-item__shipping"]') %>% 
+  html_text2()
+  
+  
+  
 
