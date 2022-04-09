cb35b0b88780f38e3b99e372c2e8ba1506dd1324
Diego Muñoz <dimunozu@gmail.com>
2022-04-09 12:47:34 -0400

    función final

--- Actividad 1.R
+++ Actividad 1.R
@@ -111,10 +111,87 @@ df <- tibble(Nombre         = Nombre,
              URL)
 
 
+# ==================================================================================================
+
+# Función final 
 
 
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1#origin=qcat&filter_applied=promotion_type&filter_position=2')
 
 
+N_Paginas <- pagina %>% 
+  html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+  html_children() %>% 
+  html_text2() %>% 
+  as.numeric() %>% 
+  max(na.rm = TRUE) %>% 
+  suppressWarnings()
+
+
+df <- 1:N_Paginas %>% 
+  map_dfr(.f = function(y){
+    
+    pagina <- read_html(paste0('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=',y))
+    
+    
+    Nombre <- pagina %>% 
+      html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+      html_text2()
+
+    oldprice <- pagina %>% 
+      html_elements(., xpath = '//span[@class = "promotion-item__oldprice"]') %>% 
+      html_text2(.) %>% 
+      str_remove_all(., '\\$ ') %>% 
+      str_remove_all(.,'\\.') %>% 
+      as.numeric(.)
+    
+    
+    newprice <- pagina %>% 
+      html_elements(xpath = '//span[@class = "promotion-item__price"]') %>% 
+      html_text2() %>% 
+      str_remove_all('\\$ ') %>% 
+      str_remove_all('\\.') %>% 
+      as.numeric()
+
+    Envio_Gratis <- 1:length(Nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__shipping"]')) %>% 
+          html_text2() %>% 
+          if_else(is.na(.),'Sin envio gratis',.) %>% 
+          if_else(identical(character(0),.),'Sin envio gratis',.)
+      })
+    
+    
+    Envio_FULL <- 1:length(Nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//svg[@class = "full-icon"]')) %>% 
+          html_text2() %>% 
+          is.na() %>% 
+          if_else('Sin envio full','Con envio full')
+      })
+    
+    
+    URL <- pagina %>% 
+      html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+      html_attr('href')
+    
+    
+    Sys.sleep(runif(1,1,2))
+    
+    
+    tibble(Nombre         = Nombre,
+           Precio_antiguo = oldprice,
+           newprice,
+           Envio_Gratis,
+           Envio_FULL,
+           URL)
+    
+  })
+
+
+df
 
 
 
